af6db05ff0217af7f8f8a6f2ad8bc562b0c659b3
hdfaouz <hdfaouzia2@gmail.com>
2025-07-08 12:02:04 +0100

    Structure de Projet

---
+++ Apprenant/src/test/java/com/enaa/apprenant/ApprenantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enaa.apprenant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApprenantApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ Rendu/src/test/java/com/enaa/rendu/RenduApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enaa.rendu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RenduApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
