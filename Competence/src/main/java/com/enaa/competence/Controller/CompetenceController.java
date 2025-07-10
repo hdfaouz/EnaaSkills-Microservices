73f89dc77933c53de01cef1ac23f18262ef17cdb
hdfaouz <hdfaouzia2@gmail.com>
2025-07-10 17:26:28 +0100

    Fonction pour ajouter Competence

--- Competence/src/main/java/com/enaa/competence/Controller/CompetenceController.java
+++ Competence/src/main/java/com/enaa/competence/Controller/CompetenceController.java
@@ -1,4 +1,24 @@
 package com.enaa.competence.Controller;
 
+import com.enaa.competence.Dto.CompetenceDto;
+import com.enaa.competence.Service.CompetenceService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/competence")
 public class CompetenceController {
+
+    private final CompetenceService competenceService;
+
+    public CompetenceController(CompetenceService competenceService) {
+        this.competenceService = competenceService;
+    }
+
+    @PostMapping
+    public CompetenceDto ajouterCompetence(@RequestBody CompetenceDto competenceDto){
+        return competenceService.ajouterCompetence(competenceDto);
+    }
 }
--- Competence/src/main/java/com/enaa/competence/Service/CompetenceService.java
+++ Competence/src/main/java/com/enaa/competence/Service/CompetenceService.java
@@ -1,4 +1,25 @@
 package com.enaa.competence.Service;
 
+import com.enaa.competence.Dto.CompetenceDto;
+import com.enaa.competence.Mappers.CompetenceMap;
+import com.enaa.competence.Model.Competence;
+import com.enaa.competence.Repository.CompetenceRepository;
+import org.springframework.stereotype.Service;
+
+@Service
 public class CompetenceService {
+
+    private final CompetenceMap competenceMap;
+    private final CompetenceRepository competenceRepository;
+
+    public CompetenceService(CompetenceMap competenceMap, CompetenceRepository competenceRepository) {
+        this.competenceMap = competenceMap;
+        this.competenceRepository = competenceRepository;
+    }
+
+    public CompetenceDto ajouterCompetence(CompetenceDto competenceDto){
+        Competence competence = competenceMap.toEntity(competenceDto);
+        Competence saveCompetence = competenceRepository.save(competence);
+        return competenceMap.toDTO(saveCompetence);
+    }
 }
