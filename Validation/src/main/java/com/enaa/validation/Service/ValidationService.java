2984e7b621754e35b3b74a141416d2d9bbd4628b
maryam mrg <maryamelouadaa@gmail.com>
2025-07-13 23:54:27 +0100

    create validation service

---
+++ Validation/src/main/java/com/enaa/validation/Service/ValidationService.java
@@ -0,0 +1,20 @@
+package com.enaa.validation.Service;
+
+import com.enaa.validation.Mapper.ValidationMapper;
+import com.enaa.validation.Repository.ValidationRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ValidationService {
+
+    private final ValidationRepository validationRepository;
+    private final ValidationMapper validationMapper;
+
+    public ValidationService(ValidationRepository validationRepository, ValidationMapper validationMapper) {
+        this.validationRepository = validationRepository;
+        this.validationMapper = validationMapper;
+    }
+
+
+    
+}
