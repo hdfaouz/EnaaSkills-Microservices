f34949285414f06846f6d1ac2d61961792b31b3a
maryam mrg <maryamelouadaa@gmail.com>
2025-07-14 14:47:43 +0100

    validation entity upadte

--- Validation/src/main/java/com/enaa/validation/Repository/ValidationRepository.java
+++
@@ -1,7 +0,0 @@
-package com.enaa.validation.Repository;
-
-import com.enaa.validation.Model.Validation;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ValidationRepository extends JpaRepository<Validation,Long > {
-}
--- Validation/src/test/java/com/example/validation/ValidationApplicationTests.java
+++ Validation/src/test/java/com/example/validation/ValidationApplicationTests.java
@@ -1,4 +1,4 @@
-package com.enaa.validation;
+package com.example.validation;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
