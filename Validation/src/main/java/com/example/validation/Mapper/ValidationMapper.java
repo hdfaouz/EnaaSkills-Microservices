3b49f328cd6368b617b0f32fee0859830184a14c
maryam mrg <maryamelouadaa@gmail.com>
2025-07-15 15:49:21 +0100

    merging

--- Validation/src/main/java/com/example/validation/Mapper/ValidationMapper.java
+++ Validation/src/main/java/com/example/validation/Mapper/ValidationMapper.java
@@ -2,17 +2,14 @@ package com.example.validation.Mapper;
 
 import com.example.validation.Dto.ValidationDto;
 import com.example.validation.Model.Validation;
-import com.example.validation.ValidationApplication;
-import org.mapstruct.Mapper;
 
 import java.util.List;
 
-@Mapper
 public interface ValidationMapper {
 
-Validation toEntity(ValidationDto dto);
+    Validation toEntity(ValidationDto dto);
 
-ValidationDto toDto(Validation validation);
+    ValidationDto toDto(Validation validation);
 
-List<ValidationDto> toDos(List<Validation> validations);
+    List<ValidationDto> toDtos(List<Validation> validations);
 }
