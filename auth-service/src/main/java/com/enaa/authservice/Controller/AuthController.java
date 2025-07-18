4b52451bdd982d70c7e272cad2efb15acc377d91
hdfaouz <hdfaouzia2@gmail.com>
2025-07-18 09:49:51 +0100

    creation de SecurityConfig

--- auth-service/src/main/java/com/enaa/authservice/Controller/AuthController.java
+++ auth-service/src/main/java/com/enaa/authservice/Controller/AuthController.java
@@ -1,4 +1,25 @@
 package com.enaa.authservice.Controller;
 
+import com.enaa.authservice.Service.AuthService;
+import com.enaa.authservice.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
 public class AuthController {
+    @Autowired
+    private AuthService service;
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @PostMapping("/register")
+    public User addNewUser(@RequestBody User user) {
+        return service.saveUser(user);
+    }
 }
