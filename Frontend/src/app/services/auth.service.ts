da3f7df300b0e0c03d56651493c4585b805aa233
hdfaouz <hdfaouzia2@gmail.com>
2025-07-31 12:07:21 +0100

    creation de authService

--- Frontend/src/app/services/auth.service.ts
+++ Frontend/src/app/services/auth.service.ts
@@ -1,9 +1,30 @@
 import { Injectable } from '@angular/core';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {Observable} from 'rxjs';
+
+interface Payload{
+  name: string,
+  email: string,
+  password: string,
+  role: string
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  private authUrl = "http://localhost:8084/api/v1/auth";
+
+
+  constructor(private http: HttpClient) { }
+
+  private getHeaders(): HttpHeaders {
+    const token = localStorage.getItem('token');
+    return new HttpHeaders().set('Authorization', `Bearer ${token}`);
+  }
+
+  register(payload: Payload): Observable<Payload> {
+    return this.http.post<Payload>(`${this.authUrl}/register`, payload);
+  }
 }
