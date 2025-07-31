148008441d63a8be39757d20d04d5d7944cc65d6
hdfaouz <hdfaouzia2@gmail.com>
2025-07-31 09:51:19 +0100

    ajouter docker file du projet et generer apprenant component

---
+++ Frontend/src/app/apprenant/apprenant.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-apprenant',
+  standalone: true,
+  imports: [],
+  templateUrl: './apprenant.component.html',
+  styleUrl: './apprenant.component.css'
+})
+export class ApprenantComponent {
+
+}
