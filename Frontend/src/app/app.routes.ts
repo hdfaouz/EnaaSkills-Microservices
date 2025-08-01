1117e1e6af2f8d69f033423160ec06e3038ea981
SamiChafik <sami.chafik00@gmail.com>
2025-08-01 17:07:39 +0100

    routes

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import {RegisterComponent} from './register/register.component';
+import { BriefListComponent } from './components/brief-list/brief-list';
 
 export const routes: Routes = [
-  {path: "", component : RegisterComponent}
+  {path: "", component : RegisterComponent},
+  {path: "brief", component: BriefListComponent}
 ];
